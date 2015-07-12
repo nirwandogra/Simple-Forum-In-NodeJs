558e848cd23379eda73ded5543e6573a4b0f035a
Nirwan Dogra <nirwandogra@gmail.com>
2015-07-12 17:53:26 +0530

    2nd

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ var sess = require('./session');
 var model = require('model');
 var connection = model.conn;
 app.use(session({
-  cookieName: 'session',
-  secret: 'random_string_goes_here',
-  duration: 30 * 60 * 1000,
-  activeDuration: 5 * 60 * 1000,
+	cookieName: 'session',
+	secret: 'random_string_goes_here',
+	duration: 30 * 60 * 1000,
+	activeDuration: 5 * 60 * 1000,
 }));
 app.use(bodyParser());
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.redirect('/login');
+	res.redirect('/login');
 });
 require('./routes')(app);
 app.listen(port);
